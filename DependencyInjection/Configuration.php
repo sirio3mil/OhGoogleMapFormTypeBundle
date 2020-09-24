185bfda1e628cbaa6a1b3c566209818809d0441b
Reynier de la Rosa <sirio3mil@users.noreply.github.com>
2020-09-24 14:48:12 +0200

    Update Configuration.php

--- DependencyInjection/Configuration.php
+++ DependencyInjection/Configuration.php
@@ -18,9 +18,14 @@ class Configuration implements ConfigurationInterface
      */
     public function getConfigTreeBuilder()
     {
-        $treeBuilder = new TreeBuilder();
-        
-        $rootNode = $treeBuilder->root('oh_google_map_form_type');
+        $treeBuilder = new TreeBuilder('oh_google_map_form_type');
+
+        if (method_exists($treeBuilder, 'getRootNode')) {
+            $rootNode = $treeBuilder->getRootNode();
+        } else {
+            // for symfony/config 4.1 and older
+            $rootNode = $treeBuilder->root('oh_google_map_form_type');
+        }
 
         // Here you should define the parameters that are allowed to
         // configure your bundle. See the documentation linked above for
